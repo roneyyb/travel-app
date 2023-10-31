64242f17d0edb51977f2fe7bd80c17d20a9b8b1f
roneyyb <bothra.rajat08@gmail.com>
2023-10-31 17:47:12 +0530

    Added navbar footer and assets

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        // Use semantic text as it helps in seo and boosting website rank
+        <nav className='border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5'>
+            <Link href="/">
+
+            </Link>
+        </nav>
+    )
+}
+
+export default NavBar
